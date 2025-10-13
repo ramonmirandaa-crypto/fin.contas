c01043e49672a5d1991569a2598ecf09745d3dec
ramonmirandaa-crypto <ramonmirandaa@gmail.com>
2025-10-13 08:59:13 -0300

    Drop recursive updated_at triggers

---
+++ migrations/0002_drop_recursive_triggers.sql
@@ -0,0 +1,12 @@
+PRAGMA foreign_keys = ON;
+
+DROP TRIGGER IF EXISTS accounts_updated_at;
+DROP TRIGGER IF EXISTS transactions_updated_at;
+DROP TRIGGER IF EXISTS budgets_updated_at;
+DROP TRIGGER IF EXISTS goals_updated_at;
+DROP TRIGGER IF EXISTS credit_cards_updated_at;
+DROP TRIGGER IF EXISTS investments_updated_at;
+DROP TRIGGER IF EXISTS loans_updated_at;
+DROP TRIGGER IF EXISTS credit_card_bills_updated_at;
+DROP TRIGGER IF EXISTS transaction_categories_updated_at;
+DROP TRIGGER IF EXISTS expenses_updated_at;
